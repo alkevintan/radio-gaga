f48a36fffbb200d5da72246103a882593908acd8
Al Kevin Tan <alkevintan@gmail.com>
2026-05-01 08:55:44 +0800

    feat: rename app to Radio Gaga, show name on splash screen
    - app_name changed from 'Radio Player' to 'Radio Gaga'
    - Splash screen now shows the icon + 'Radio Gaga' text below
    - Toolbar title references @string/app_name

--- app/src/main/java/com/radio/player/ui/SplashActivity.kt
+++ app/src/main/java/com/radio/player/ui/SplashActivity.kt
@@ -1,6 +1,5 @@
 package com.radio.player.ui
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -9,12 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.radio.player.MainActivity
 import com.radio.player.util.SettingsManager
 
-@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         applyTheme()
         super.onCreate(savedInstanceState)
+        setContentView(com.radio.player.R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
@@ -24,9 +23,9 @@ class SplashActivity : AppCompatActivity() {
 
     private fun applyTheme() {
         when (SettingsManager.getTheme(this)) {
-            SettingsManager.Theme.LIGHT -> setTheme(com.radio.player.R.style.Theme_RadioPlayer_Splash)
-            SettingsManager.Theme.DARK -> setTheme(com.radio.player.R.style.Theme_RadioPlayer_Splash)
-            SettingsManager.Theme.SYSTEM -> setTheme(com.radio.player.R.style.Theme_RadioPlayer_Splash)
+            SettingsManager.Theme.LIGHT -> setTheme(com.radio.player.R.style.Theme_RadioPlayer)
+            SettingsManager.Theme.DARK -> setTheme(com.radio.player.R.style.Theme_RadioPlayer)
+            SettingsManager.Theme.SYSTEM -> setTheme(com.radio.player.R.style.Theme_RadioPlayer)
         }
     }
 }
